3e13d93bc66d0e4c4408ad713ee5290913b6f188
Nick Urch <nmu0901@yahoo.com>
2025-01-10 16:41:29 -0500

    Style Changes

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -15,20 +15,13 @@ export default function Dice(props) {
     
     function getDieImage() {
         switch (props.value) {
-            case 1:
-                return One
-            case 2:
-                return Two
-            case 3:
-                return Three
-            case 4:
-                return Four
-            case 5:
-                return Five
-            case 6:
-                return Six
-            default:
-                return Unkown
+            case 1: return One
+            case 2: return Two
+            case 3: return Three
+            case 4: return Four
+            case 5: return Five
+            case 6: return Six
+            default: return Unkown
         }
     }
 
